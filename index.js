e45bf8acd7114067e8a3c92b2def513abf62899d
ovie <usmanovie128@gmail.com>
2025-03-05 16:29:25 +0100

    updated index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+import 'dotenv/config';
+import express from 'express';
+import connectDB from './config/db.js';
+//import from controller comes here
+import dotenv from 'dotenv';
+dotenv.config(); // Load environment variables from .env file
+connectDB();
+
+
+const app = express();
+// middleware
+app.use(express.json());
+// Use routes
+app.use
+app.use
+app.use
+
+
+
+console.log('MongoDB URI:', process.env.MONGO_URI); // Check if the URI is defined
+
+
+// Set up the server to listen on a port
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, ()  => console.log( `Server running on  http://localhost:${PORT}`));
